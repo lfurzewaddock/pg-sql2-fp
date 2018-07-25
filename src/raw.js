e95c243d89a68a68361414bf59f4037dc8335020
Leo Furze-Waddock <leo@furze-waddock.com>
2018-07-25 17:20:25 +0100

    refactor raw and remove redundant tests

--- src/raw.js
+++ src/raw.js
@@ -1,11 +1,16 @@
 import trustedSymbol from "./trusted-symbol";
 
 function makeRawNode(text = "", symbol) {
+  var trustSymbol = symbol;
   var rawText = text;
+
+  if (typeof symbol === "undefined" || typeof symbol !== "symbol") {
+    trustSymbol = trustedSymbol();
+  }
   if (typeof text !== "string") {
     rawText = String(text);
   }
-  return { type: "RAW", text: rawText, [symbol]: true };
+  return { type: "RAW", text: rawText, [trustSymbol]: true };
 }
 
 /**
@@ -13,10 +18,6 @@ function makeRawNode(text = "", symbol) {
  * Creates a Sql item for raw SQL text. Just plain ol‘ raw SQL.
  * This method is dangerous because it involves no escaping.
  */
-export default function raw(text, symbol) {
-  var trustSymbol = symbol;
-  if (typeof symbol === "undefined" || typeof symbol !== "symbol") {
-    trustSymbol = trustedSymbol();
-  }
-  return makeRawNode(text, trustSymbol);
+export default function raw(text) {
+  return makeRawNode(text);
 }
--- test/raw.test.js
+++ test/raw.test.js
@@ -1,6 +1,5 @@
 import test from "tape";
 import raw from "../src/raw";
-import trustedSymbol from "../src/trusted-symbol";
 
 test("raw", (t) => {
   t.test("raw", (assert) => {
@@ -34,78 +33,11 @@ test("raw", (t) => {
 
     assert.end();
   });
-  t.test("invoke raw with a string as the 1st argument and 'undefined' as the 2nd argument", (assert) => {
-    const node = raw("string", undefined);
-
-    const message = "should return a SQL node (type 'RAW') with text property set to the 1st argument";
-    const expected = { type: "RAW", text: "string" };
-    const actual = node;
-
-    assert.deepEqual(actual, expected, message);
-
-    assert.end();
-  });
-  t.test("invoke raw with a string as the 1st argument and a string as the 2nd argument", (assert) => {
-    const node = raw("string", "string");
-
-    const message = "should return a SQL node (type 'RAW') with text property set to the 1st argument";
-    const expected = { type: "RAW", text: "string" };
-    const actual = node;
-
-    assert.deepEqual(actual, expected, message);
-
-    assert.end();
-  });
   t.test("invoke raw with an integer as the 1st argument", (assert) => {
     const node = raw(1);
 
     const message = "should return a SQL node (type 'RAW') with text property set to the 1st argument converted to a string";
     const expected = { type: "RAW", text: "1" };
-    const actual = node;
-
-    assert.deepEqual(actual, expected, message);
-
-    assert.end();
-  });
-  t.test("invoke raw with an integer as the 1st argument and 'undefined' as the 2nd argument", (assert) => {
-    const node = raw(1, undefined);
-
-    const message = "should return a SQL node (type 'RAW') with text property set to the 1st argument converted to a string";
-    const expected = { type: "RAW", text: "1" };
-    const actual = node;
-
-    assert.deepEqual(actual, expected, message);
-
-    assert.end();
-  });
-  t.test("invoke raw with an integer as the 1st argument and a 'string' as the 2nd argument", (assert) => {
-    const node = raw(1, "string");
-
-    const message = "should return a SQL node (type 'RAW') with text property set to the 1st argument converted to a string";
-    const expected = { type: "RAW", text: "1" };
-    const actual = node;
-
-    assert.deepEqual(actual, expected, message);
-
-    assert.end();
-  });
-  t.test("invoke raw with an integer as the 1st argument and a 'number object' as the 2nd argument", (assert) => {
-    const node = raw(1, new Number(1)); /* eslint-disable-line no-new-wrappers */
-
-    const message = "should return a SQL node (type 'RAW') with text property set to the 1st argument converted to a string";
-    const expected = { type: "RAW", text: "1" };
-    const actual = node;
-
-    assert.deepEqual(actual, expected, message);
-
-    assert.end();
-  });
-  t.test("invoke raw with a string as the 1st argument and a 'Symbol' as the 2nd argument", (assert) => {
-    const ident = Symbol.for("trusted");
-    const node = raw("string", ident);
-
-    const message = "should return an object with two properties: text property: SQL query with alias __local_[increment]__ and values property: empty array ";
-    const expected = { type: "RAW", text: "string" };
     const actual = node;
 
     assert.deepEqual(actual, expected, message);
