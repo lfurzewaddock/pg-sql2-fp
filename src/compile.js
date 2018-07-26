95915c39bf754c9505919b558ea48938e05becc1
lfurzewaddock <leo@furze-waddock.com>
2018-07-26 19:07:01 +0100

    breakout handleSqlIdentifier into module

--- src/compile.js
+++ src/compile.js
@@ -1,40 +1,5 @@
-import debugError from "./debug-error";
 import enforceValidNode from "./enforce-valid-node";
-import escapeSqlIdentifier from "./escape-sql-identifier";
-
-function handleIdentifier(names) {
-  // When we come accross a symbol in our identifier, we create a unique
-  // alias for it that shouldn’t be in the users schema. This helps maintain
-  // sanity when constructing large Sql queries with many aliases.
-  let nextSymbolId = 0;
-
-  const symbolToIdentifier = new Map();
-
-  const mapResult = names.map((name) => {
-    if (typeof name === "string") {
-      return escapeSqlIdentifier(name);
-    }
-
-    if (typeof name === "symbol") {
-      // Get the correct identifier string for this symbol.
-      let identifierSymbol = symbolToIdentifier.get(name); // get value associated to name
-
-      // If there is no identifier, create one and set it.
-      if (typeof identifierSymbol === "undefined") {
-        identifierSymbol = `__local_${nextSymbolId += 1}__`; // Manualy create symbol with magic string and increment
-        symbolToIdentifier.set(name, identifierSymbol); // set value for the key in the Map object.
-      }
-
-      // Return the identifier. As we created it, we do not have to
-      // escape it, because we know all of the characters are safe.
-      return identifierSymbol;
-    }
-
-    throw debugError(new Error(`Expected string or symbol, received '${String(name)}'`));
-  }).join(".");
-
-  return mapResult;
-}
+import handleSqlIdentifier from "./handle-sql-identifier";
 
 export default function compile(sql) {
   // Join this to generate the SQL query
@@ -64,7 +29,7 @@ export default function compile(sql) {
         }
         // If we got an identifier type, escape the strings and get a local
         // identifier for non-string identifiers.
-        sqlFragments.push(handleIdentifier(item.names));
+        sqlFragments.push(handleSqlIdentifier(item.names));
         break;
       case "VALUE":
         // If we got a value SQL item, add a placeholder and add the value to our
