bb1643f41647383954263f178056ef02007af0fd
Leo Furze-Waddock <leo@furze-waddock.com>
2018-08-13 17:46:58 +0100

    drop support for web browser - not required

--- test/jsdom.environment.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const jsdom = require("jsdom");
-
-// Add 'jsdom' to global object under 'kitTddJsLfurzewaddockComGithub' namespace
-const kitTddJsLfurzewaddockComGithub = global.kitTddJsLfurzewaddockComGithub || {};
-kitTddJsLfurzewaddockComGithub.jsdom = jsdom;
-global.kitTddJsLfurzewaddockComGithub = kitTddJsLfurzewaddockComGithub;
