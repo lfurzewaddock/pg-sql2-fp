f738ef0cf0d4c7280064f9d7862f2ab69fbb58a9
Leo Furze-Waddock <leo@furze-waddock.com>
2018-11-26 22:47:37 +0000

    fix: set NODE_ENV to prod if undefined

--- src/trusted-symbol.js
+++ src/trusted-symbol.js
@@ -1,4 +1,6 @@
 /* istanbul ignore next */
+if (!process.env.NODE_ENV) process.env.NODE_ENV = "production";
+
 const $$trusted = process.env.NODE_ENV === "production" ? Symbol("trusted") : Symbol.for("development");
 
 export default function trustedSymbol() {
